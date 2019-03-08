aae5228951eb06ea54b121a3f3acdf76995d9088
ParkerMJ <parkermattjohnson@gmail.com>
2019-03-07 20:44:48 -0800

    added GIPHY integration into js

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,3 @@
 //javascript, jQuery
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=N3CI0neky6WXR0qvwv4SykuGWuQFL650=5");
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=winner&api_key=N3CI0neky6WXR0qvwv4SykuGWuQFL650=5");
 xhr.done(function(data) { console.log("success got data", data); });
