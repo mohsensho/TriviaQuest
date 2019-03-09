0922c3b1ef1e846f7d58d4bdbf47a811b014d03b
SheriffHobo <zombiemaster@gmail.com>
2019-03-08 17:56:25 -0800

    lots of updates to index, some css, and added one function in js

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+$(function() {
+
+    $(".title-letter").hover(function(){
+        var r = Math.floor(Math.random() * 255);
+        var g = Math.floor(Math.random() * 255);
+        var b = Math.floor(Math.random() * 255);
+        var color = "rgb("+r+","+g+","+b+")"
+        $(this).css("color", color);
+    });
+
+});
